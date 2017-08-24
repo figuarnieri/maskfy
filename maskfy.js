2388badebe65a8290dab530caa663f4b38120db3
Filipe Guarnieri <fyguarnieri@gmail.com>
2017-08-24 15:54:29 -0300

    Min size numbers

--- maskfy.js
+++ maskfy.js
@@ -7,8 +7,9 @@ function Maskfy( object ) {
             const input = e.target
             , inputValue =  input.value
             , inputLength =  inputValue.length
-            , mask = input.dataset.mask
+            , mask = object.mask || input.dataset.mask
             , maskLength = mask.length
+            , maskSize = ( object.size || tag.dataset.maskSize )
             , value = inputValue.replace(/\D/g, '')
             , valueFinal = value.split('')
             , objectReverse = object.reverse || input.hasAttribute('data-mask-reverse')
@@ -18,13 +19,21 @@ function Maskfy( object ) {
                 valueFinal.pop();
             }
             if( objectReverse ){
+                if( /\d/.test(valueFinal[0]) && valueFinal.length>maskSize && valueFinal[0]==='0'){
+                    valueFinal.shift();
+                }
                 for( i in inputValue.split('') ){
-                    if( /\D/.test(mask[maskLength - i - 1]) ) {
-                        valueFinal.splice(valueFinal.length - i, 0, mask[maskLength - i - 1]);
+                    if( /\D/.test(mask[maskLength-i-1]) ) {
+                        valueFinal.splice(valueFinal.length-i, 0, mask[maskLength-i-1]);
                     }
                 }
-                if ( (e.inputType==='deleteContentBackward') && (/\D/.test(valueFinal[0])) ) {
-                    valueFinal.shift();
+                if( (e.inputType==='deleteContentBackward') ){
+                    if( (value.length<maskSize) ){
+                        valueFinal.unshift(0);
+                    }
+                    if( (/\D/.test(valueFinal[0])) ){
+                        valueFinal.shift();
+                    }
                 }
             } else {
                 for( i in inputValue.split('') ){
@@ -32,7 +41,7 @@ function Maskfy( object ) {
                         valueFinal.splice(i, 0, mask[i]);
                     }
                 }
-                if ( e.inputType==='deleteContentBackward' ) {
+                if( e.inputType==='deleteContentBackward' ) {
                     for( i=valueFinal.length-1; i>=0 ; i-- ){
                         (/\D/.test(valueFinal[i])) ? valueFinal.pop() : i=-1;
                     }
@@ -46,14 +55,18 @@ function Maskfy( object ) {
                 }, 0);
             }
         });
-
-        const inputTrigger = tag.value.split('');
+        const inputTrigger = tag.value.split('')
+        , inputLength = inputTrigger.length
+        , objectSize = ( object.size || tag.dataset.maskSize || 0 )
+        ;
+        for( var i=objectSize; i>inputLength; i-- ){
+            inputTrigger.unshift(0);
+        }
         tag.value = '';
         inputTrigger.map((item) => {
-            tag.value+=item;
+            tag.value += item;
             const eventTrigger = new MouseEvent('input');
             tag.dispatchEvent(eventTrigger);
         });
     });
 }
-Maskfy('[data-mask]');
