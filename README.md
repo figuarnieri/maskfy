f3c0e9e7f21c0cb14e02c60fc79eb759f394cf08
Filipe Guarnieri <filipe@ibaro.com.br>
2017-12-18 17:22:09 -0200

    Maskfy Class JS

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# MASKFY
-Input mask + ES6 + no dependence + **only 1kb** (minify) = ♥
+# Maskfy - Input Mask Simple
+A Javascript library without a dependency of jQuery, Zepto, and etc ... Very simple to install and use. With only 1kb (gzip) code, it's also well accepted on mobile devices
+## Install / Usage
+Add Maskfy file
+```html
+<script src="path/js/maskfy.min.js"></script>
+```
+### HTML _(by class or attribute)_
+Insert a selector in class tag
+```html
+<input class="page--input" data-mask="999.999.999-99" type="text" placeholder="CPF (Ex.: 999.999.999-99)">
+or
+<input class="page--input mask-by-class" type="text" placeholder="CPF (Ex.: 999.999.999-99)">
+```
+### Javascript Simple _(by attribute)_
+```html
+<script>
+  new Maskfy('[data-mask]');
+</script>
+or
+<script>
+  new Maskfy('.mask-by-class');
+</script>
+```
+### Javascript Options
+```html
+<script>
+  new Maskfy({
+    tag: '[data-mask]', // selector tag
+    reverse: true, // reverse typing
+    mask: '999.999.999,99', // mask pattern
+    size: 3 // minimum digits
+  });
+</script>
+```
+## Data attributes _[data-*]_
+If necessary, you could add attributes in the tag, to configure your mask options
+
+### data-mask
+```html
+<input data-mask="999.999.999,99">
+```
+### data-mask-reverse
+```html
+<input data-mask="999.999.999,99" data-mask-reverse>
+```
+### data-mask-size
+```html
+<input data-mask="999.999.999,99" data-mask-reverse="" data-mask-size>
+```
+
+## Source
+[Github](https://github.com/figuarnieri/maskfy) | [@figuarnieri](https://twitter.com/figuarnieri)
+
+## License
+The MIT License
+Copyright 2017 © [Filipe Guarnieri](https://figuarnieri.github.io/)
--- maskfy.js
+++ maskfy.js
@@ -1,72 +1,91 @@
 "use strict";
-function Maskfy( object ) {
-    const objectTag = typeof object==='string' ? object : object.tag;
-    document.querySelectorAll(objectTag).forEach((tag) => {
-        tag.addEventListener('paste', (e) => e.preventDefault());
-        tag.addEventListener('input', (e) => {
-            const input = e.target
-            , inputValue =  input.value
-            , inputLength =  inputValue.length
-            , mask = object.mask || input.dataset.mask
-            , maskLength = mask.length
-            , maskSize = ( object.size || tag.dataset.maskSize )
-            , value = inputValue.replace(/\D/g, '')
-            , valueFinal = value.split('')
-            , objectReverse = object.reverse || input.hasAttribute('data-mask-reverse')
-            ;
-            var i;
-            if( inputLength > maskLength ){
-                valueFinal.pop();
-            }
-            if( objectReverse ){
-                if( /\d/.test(valueFinal[0]) && valueFinal.length>maskSize && valueFinal[0]==='0'){
-                    valueFinal.shift();
-                }
-                for( i in inputValue.split('') ){
-                    if( /\D/.test(mask[maskLength-i-1]) ) {
-                        valueFinal.splice(valueFinal.length-i, 0, mask[maskLength-i-1]);
-                    }
-                }
-                if( (e.inputType==='deleteContentBackward') ){
-                    if( (value.length<maskSize) ){
-                        valueFinal.unshift(0);
-                    }
-                    if( (/\D/.test(valueFinal[0])) ){
-                        valueFinal.shift();
-                    }
-                }
-            } else {
-                for( i in inputValue.split('') ){
-                    if( /\D/.test(mask[i]) ) {
-                        valueFinal.splice(i, 0, mask[i]);
-                    }
-                }
-                if( e.inputType==='deleteContentBackward' ) {
-                    for( i=valueFinal.length-1; i>=0 ; i-- ){
-                        (/\D/.test(valueFinal[i])) ? valueFinal.pop() : i=-1;
-                    }
-                }
-            }
-            input.value = valueFinal.join('');
-            if(window.innerWidth < 760){
-                setTimeout(() => {
-                    input.selectionStart = input.selectionEnd = input.value.length;
-                    input.focus();
-                }, 0);
-            }
-        });
-        const inputTrigger = tag.value.split('')
-        , inputLength = inputTrigger.length
-        , objectSize = ( object.size || tag.dataset.maskSize || 0 )
-        ;
-        for( var i=objectSize; i>inputLength; i-- ){
-            inputTrigger.unshift(0);
-        }
-        tag.value = '';
-        inputTrigger.map((item) => {
-            tag.value += item;
-            const eventTrigger = new MouseEvent('input');
-            tag.dispatchEvent(eventTrigger);
-        });
+
+class Maskfy {
+  constructor(object) {
+    this.objectTag = typeof object==='string' ? object : object.tag;
+    this.objectSize = object.size;
+    this.objectMask = object.mask;
+    this.objectReverse = object.reverse;
+    this.init(this.objectTag);
+  }
+  init(input) {
+    const _this = this;
+    document.querySelectorAll(input).forEach((tag) => {
+      _this.paste(tag);
+      _this.input(tag);
+      _this.format(tag);
     });
+  }
+  input(input) {
+    const _this = this;
+    input.addEventListener('input', (e) => {
+      const inputValue =  input.value
+      , inputLength =  inputValue.length
+      , mask = _this.objectMask || input.dataset.mask
+      , maskLength = mask.length
+      , maskSize = ( _this.objectSize || input.dataset.maskSize )
+      , value = inputValue.replace(/\D/g, '')
+      , valueFinal = value.split('')
+      , objectReverse = _this.objectReverse || input.hasAttribute('data-mask-reverse')
+      ;
+      var i;
+      if( inputLength > maskLength ) {
+        valueFinal.pop();
+      }
+      if( objectReverse ) {
+        if( /\d/.test(valueFinal[0]) && valueFinal.length>maskSize && valueFinal[0]==='0') {
+          valueFinal.shift();
+        }
+        for( i in inputValue.split('') ) {
+          if( /\D/.test(mask[maskLength-i-1]) ) {
+            valueFinal.splice(valueFinal.length-i, 0, mask[maskLength-i-1]);
+          }
+        }
+        if( (e.inputType==='deleteContentBackward') ) {
+          if( (value.length<maskSize) ) {
+            valueFinal.unshift(0);
+          }
+          if( (/\D/.test(valueFinal[0])) ) {
+            valueFinal.shift();
+          }
+        }
+      } else {
+        for( i in inputValue.split('') ) {
+          if( /\D/.test(mask[i]) ) {
+            valueFinal.splice(i, 0, mask[i]);
+          }
+        }
+        if( e.inputType==='deleteContentBackward' ) {
+          for( i=valueFinal.length-1; i>=0 ; i-- ) {
+            (/\D/.test(valueFinal[i])) ? valueFinal.pop() : i=-1;
+          }
+        }
+      }
+      input.value = valueFinal.join('');
+      _this.responsive(input);
+    });
+  }
+  paste(input) {
+    input.addEventListener('paste', (e) => e.preventDefault());
+  }
+  format(input) {
+    const inputTrigger = input.value.split('')
+    , inputLength = inputTrigger.length
+    , objectSize = ( this.objectSize || input.dataset.maskSize || 0 )
+    ;
+    for( var i=objectSize; i>inputLength; i-- ) {
+      inputTrigger.unshift(0);
+    }
+    input.value = '';
+    inputTrigger.map((item) => {
+      input.value += item;
+      const eventTrigger = new MouseEvent('input');
+      input.dispatchEvent(eventTrigger);
+    });
+  }
+  responsive(input) {
+    setTimeout(() => {
+      input.selectionStart = input.selectionEnd = input.value.length;
+    }, 0);
+  }
 }
