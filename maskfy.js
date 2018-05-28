af6bb7a65e292665c99d3889f43f5aee7a3a61ff
João Ribeiro <jlribeiroaugusto@gmail.com>
2018-05-28 12:09:07 -0300

    add amd module e commomjs
    Adicionar modulos amd e commomjs para poder exportar e trabalhar com webpack.

--- maskfy.js
+++ maskfy.js
@@ -140,3 +140,24 @@ class Maskfy {
     }, 0);
   }
 }
+
+(function (root, clazz) {
+    if (typeof define === 'function' && define.amd) {
+        /**
+         * AMD. Register as an anonymous module.
+         */
+        define([], clazz);
+    } else if (typeof exports === 'object') {
+        /**
+        * Node. Does not work with strict CommonJS, but
+        * only CommonJS-like environments that support module.exports,
+        * like Node.
+        */
+        module.exports = clazz;
+    } else {
+        /**
+        * Browser globals (root is window)
+        */
+        root.memoize = clazz;
+    }
+}(this, Maskfy))
